ae7a37980e6bac81bd31a5d8fd2b88ffdc914d0f
katrina+charles <katrina+charles@appacademy.io>
2015-07-09 10:27:24 -0400

    Add backbone Feed model and collection.

---
+++ app/assets/javascripts/news_reader.js
@@ -0,0 +1,12 @@
+window.NewsReader = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+  }
+};
+
+$(document).ready(function(){
+  NewsReader.initialize();
+});
